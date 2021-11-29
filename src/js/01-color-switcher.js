1d8e1dd37b6c3922ab07c55fd3f8f1682bef710d
Igor Napolov <isnapolov@gmail.com>
2021-11-29 11:27:47 +0200

    Задание 1 - переключатель цветов

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
 
+
+const startEl = document.querySelector('button[data-start]');
+const stopEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector("body");
+let switchColor = null;
+let hasStarted = false;
+stopEl.disabled=true;
+
+startEl.addEventListener("click", colorSwitcherStart);
+
+stopEl.addEventListener("click",colorSwitcherStop)
+
+function colorSwitcherStart() {    
+    if (hasStarted) {        
+        return;
+    };
+    hasStarted = true;
+    startEl.disabled = true;
+    stopEl.disabled = false;
+        switchColor = setInterval(() => {   
+        
+        bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+        
+    }, 1000);
+    
+};
+
+function colorSwitcherStop() {
+    clearInterval(switchColor);
+    hasStarted = false;
+    startEl.disabled = false;
+    stopEl.disabled=true;
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
