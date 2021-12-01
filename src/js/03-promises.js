ef17d347cc08a7ac20cd4ddc007df493d1f3a4d6
Igor Napolov <isnapolov@gmail.com>
2021-12-01 08:28:17 +0200

    Задание 3 - генератор промисов

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const submitEl = document.querySelector(".form");
+
+
+submitEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  
+  const formElements = event.currentTarget.elements;
+  let firstDelay = Number(formElements.delay.value);
+  const delayStep = Number(formElements.step.value);
+  const amount = Number(formElements.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+
+    createPromise(i, firstDelay).then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    }).catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+    firstDelay += delayStep;
   }
+});
+
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+
+
+
